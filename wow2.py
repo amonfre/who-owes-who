623457c2fa9fdb2b393c35bc594112b1427b4b5e
William Whitman <wwhitman@college.harvard.edu>
2015-04-20 03:18:32 -0400

    Adding wow2

---
+++ wow2.py
@@ -0,0 +1,32 @@
+
+num = int(raw_input())
+
+graph = []
+friends = []
+
+for i in range(num):
+	friend = map(int, raw_input().split())
+	friend.append(i)
+	check_friends = False
+	for j in range(len(friends)):
+		if friend[0] in friends[j]:
+			check_friends = True
+		elif friend[0] == -1:
+			check_friends = True
+	if check_friends == False:
+		friends.append(friend)
+	edges = raw_input().split(" ")
+	edges = dict(map(int, edge.split(",")) for edge in edges)
+	graph.append(edges)
+
+print graph
+print friends
+
+networth = [0 for x in range(num)]
+for i in range(len(friends)):
+	for friend in friends[i]:
+		for node in graph[friend]:
+			networth[friend] -= graph[friend][node]
+			networth[node] += graph[friend][node]
+
+print networth
