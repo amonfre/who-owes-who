469ad90e6dc946d28c3aa321d4b65e98165fc7af
William Whitman <wwhitman@college.harvard.edu>
2015-04-20 08:51:31 -0400

    wow2 update

--- wow2.py
+++ wow2.py
@@ -2,6 +2,8 @@
 entries = 6
 friends = 3
 
+friendList = [1,1,1,2,2,2]
+
 Matrix = [[0 for x in range(entries)] for y in range(entries)]
 Matrix [0][1] = -10
 Matrix [0][3] = -10
@@ -15,18 +17,30 @@ Matrix [4][5] = -10
 print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
       for row in Matrix]))
 
-for i in range(friends):
-    for j in range(friends,entries):
+counter1 = 0
+newfriends = [0]
+
+while (len(friendList)) > 0:
+    firstFriend = friendList[0]
+    while len(friendList) > 0 and friendList[0] == firstFriend:
+        friendList.pop(0)
+        counter1 += 1
+    newfriends.append(counter1)
+ 
+print newfriends
+
+for i in range(newfriends[0],newfriends[1]):
+    for j in range(newfriends[1],entries):
         if Matrix[i][j] < 0 and i > 0:
-            Matrix[0][i] -= Matrix[i][j]
-            Matrix[0][j] += Matrix[i][j]
+            Matrix[newfriends[0]][i] -= Matrix[i][j]
+            Matrix[newfriends[0]][j] += Matrix[i][j]
             Matrix[i][j] = 0
-        if Matrix[i][j] > 0 and j > friends:
-            Matrix[i][friends] += Matrix[i][j]
-            Matrix[friends][j] += Matrix[i][j]
+        if Matrix[i][j] > 0 and j > newfriends[1] and j < newfriends[2]:
+            Matrix[i][newfriends[1]] += Matrix[i][j]
+            Matrix[newfriends[1]][j] += Matrix[i][j]
             Matrix[i][j] = 0
 print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in Matrix]))     
+      for row in Matrix]))
        
 for i in range(entries):
     for j in range(i):
@@ -43,3 +57,4 @@ for i in range(entries):
         Transactions += 1
 print networth
 print Transactions
+
