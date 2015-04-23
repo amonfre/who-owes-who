f8b4878521ccc29c26bd206bf7f047eff60f73d3
Alicia Monfre <amonfre@college.harvard.edu>
2015-04-23 14:47:07 -0400

    updated graph setup

--- wow.py
+++ wow.py
@@ -7,9 +7,17 @@ for i in range(num):
 	friend = map(int, raw_input().split())
 	friends.append(friend)
 	edges = raw_input().split(" ")
-	edges = dict(edge.split(",") for edge in edges)
+	edges = dict(map(int, edge.split(",")) for edge in edges)
 	graph.append(edges)
 
 print graph
 print friends
 
+networth = [0 for x in range(num)]
+for i in range(num):
+	for node in graph[i]:
+		if node != -1:
+			networth[i] -= graph[i][node]
+			networth[node] += graph[i][node]
+
+print networth
