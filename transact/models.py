bb8bc8da4def2bcb325857e03377298fc115c548
Sam Kaplan <samuelkaplan@college.harvard.edu>
2015-04-27 16:56:12 -0700

    just gots graph to work

--- transact/models.py
+++ transact/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 # Create your models here.
 
-class Transaction(models.Model):
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Transaction(BaseModel):
    created_at = models.DateField()
    recepient = models.ForeignKey(User,related_name='recepient')
    sender = models.ForeignKey(User,related_name='sender')
    amount = models.FloatField()
+   
+class TransactionForm(ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['recepient', 'sender', 'amount']
+        
     
-
