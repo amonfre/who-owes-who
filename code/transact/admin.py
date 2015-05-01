65e91f278191c1256a1a0e86dfea4d8d318a2cc7
Alicia Monfre <amonfre@college.harvard.edu>
2015-05-01 15:48:12 -0400

    cleaning up more

--- code/transact/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from models import Transaction
-
-class TransactionAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Transaction, TransactionAdmin)
-
-# Register your models here.
--- djang/transact/templatetags/myfilters.py
+++
@@ -1,9 +0,0 @@
-#https://stackoverflow.com/questions/401025/define-css-class-in-django-forms
-
-from django import template
-
-register = template.Library()
-
-@register.filter(name='addcss')
-def addcss(value, arg):
-    return value.as_widget(attrs={'class': arg})
